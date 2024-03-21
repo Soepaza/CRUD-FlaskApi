df8e6e126abd0937e2a721000ded06fedb6121bc
soepaza <soefreestyle@gmail.com>
2024-03-21 14:29:27 -0300

    project

--- execute.py
+++ execute.py
@@ -1,1 +1,32 @@
+from flask import Flask, request, jsonify
 print("Novo projeto, copia do pinterest usando Flask.")
+
+# definir aplicacao
+myApp = Flask(__name__)
+
+# ter um banco de dados para testar (vou criar um dic)
+alunos = [
+    {"nome": "soe", "id": 12, "cidade": "SP"},
+    {"nome": "carboni", "id": 10, "cidade": "RJ"},
+    {"nome": "schautz", "id": 20, "cidade": "ES"},
+]
+
+# definir rota
+
+
+@myApp.route("/alunos", methods=['GET'])
+def get_aluno():
+    return jsonify(alunos)
+
+
+@myApp.route("/alunos/<int:id_aluno>", methods=['DELETE'])
+def delete_aluno(id_aluno):
+    for jovem in alunos:
+        if jovem['id'] == id_aluno:
+            alunos.remove(jovem)
+        return "Aluno deletado com sucesso.", 200
+    return "Aluno nao encontrado.", 404
+
+
+if (__name__) == "__main__":
+    myApp.run(debug=True)
