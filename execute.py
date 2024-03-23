ea0091af1f4ecc10a7e4ad1636da21074ff73afc
soepaza <soefreestyle@gmail.com>
2024-03-23 01:40:32 -0300

    Projeto básico usando Flask e Postman

--- execute.py
+++ execute.py
@@ -6,17 +6,18 @@ myApp = Flask(__name__)
 
 # ter um banco de dados para testar (vou criar um dic)
 alunos = [
-    {"nome": "soe", "id": 12, "cidade": "SP"},
-    {"nome": "carboni", "id": 10, "cidade": "RJ"},
-    {"nome": "schautz", "id": 20, "cidade": "ES"},
+    {"nome": "ribeiro", "id": 1, "cidade": "SP"},
+    {"nome": "fernandes", "id": 2, "cidade": "RJ"},
+    {"nome": "matos", "id": 3, "cidade": "ES"},
 ]
 
+
 # definir rota
-
-
 @myApp.route("/alunos", methods=['GET'])
 def get_aluno():
     return jsonify(alunos)
+
+# Rota para obter específico por ID
 
 
 @myApp.route("/alunos/<int:id_aluno>", methods=['GET'])
@@ -24,10 +25,34 @@ def get_estudante(id_aluno):
     for aluno in alunos:
         if aluno['id'] == id_aluno:
             return jsonify(aluno)
-        
+    return jsonify({'mensagem': 'Aluno não encontrado.'}), 404
 
+# Rota para criar um novo aluno
+@myApp.route("/alunos", methods=['POST'])
+def create_aluno():
+    dados = request.json
+    if 'nome' not in dados or 'cidade' not in dados:
+        return jsonify({'mensagem': 'Nome e cidade são campos obrigatórios.'}), 400
+    novo_aluno = {
+        "nome": dados.get('nome'),
+        "id": len(alunos) + 1,
+        "cidade": dados.get('cidade')
+    }
+    alunos.append(novo_aluno)
+    return jsonify(novo_aluno), 201
 
+# Rota para atualizar os dados de um aluno existente
+@myApp.route("/alunos/<int:id_aluno>", methods=['PUT'])
+def atualizar_aluno(id_aluno):
+    dados = request.json
+    for i in alunos:
+        if i['id'] == id_aluno:
+            i['nome'] = dados.get('nome', i['nome'])
+            i['cidade'] = dados.get('cidade', i['cidade'])
+            return jsonify(i), 200
+    return jsonify({'mensagem': 'Aluno nao encontrado.'})
 
+# Rota para deletar um aluno por ID
 @myApp.route("/alunos/<int:id_aluno>", methods=['DELETE'])
 def delete_aluno(id_aluno):
     for jovem in alunos:
@@ -37,5 +62,6 @@ def delete_aluno(id_aluno):
     return "Aluno nao encontrado.", 404
 
 
+# Executar
 if (__name__) == "__main__":
     myApp.run(debug=True)
